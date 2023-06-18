c577d0fb8a85622faf4f8ca143ac01574462efeb
Yesid <yesidrodriguez305@gmail.com>
2023-06-17 20:52:47 -0500

    Add callback playground

---
+++ src/playgrounds/callback.js
@@ -0,0 +1,24 @@
+/*Tienes la función execCallback que recibirá un callback es decir una función como parámetro, tu reto es ejecutar esa función con un tiempo de demora de 2 segundos.
+
+Para hacer que la función se demore 2 segundos debes usar la función setTimeout, pero para ejecutarla debes llamarla mediante el namescpace window para poder monitorear su uso en la ejecución de pruebas, ejemplo:
+
+window.setTimeout(() => {
+  // code
+})
+
+Dentro del cuerpo de la función execCallback debes escribir tu solución.
+
+Ejemplo:
+
+Input:
+const myFunc = () => console.log('Log after 2s')
+execCallback(myFunc);
+
+Output:
+// Execute myFunc 2s after
+*/
+
+export function execCallback(callback) {
+  // Tu código aquí 👈
+  window.setTimeout(callback, 2000);
+}
