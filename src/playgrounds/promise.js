2f2ee410b68f6e6a17e4bd0088150cec06f79ef8
Yesid <yesidrodriguez305@gmail.com>
2023-06-26 00:06:02 -0500

    Add promise playground

---
+++ src/playgrounds/promise.js
@@ -0,0 +1,62 @@
+/* 
+En este desafío tienes la función delay la cual se espera que un tiempo específico retorne un mensaje
+
+La función deberá recibir dos parámetros:
+
+time: el tiempo de espera
+message: el mensaje que debe imprimir después del tiempo de espera
+La función delay debe retornar una promesa para poderlo usarlo de forma asíncrona.
+
+> Nota: Debes usar la función setTimeout con el namespace window para poder monitorear su uso en la ejecución de pruebas, ejemplo:
+
+```js
+window.setTimeout(() => {
+  // code
+})
+```
+
+Ejemplo 1:
+
+```js
+Input:
+delay(2000, "Hello after 2s")
+.then((message) => console.log(message))
+
+Output:
+// after 2s
+"Hello after 2s"
+```
+
+Ejemplo 2:
+
+```js
+Input:
+delay(3000, "Hello after 3s")
+.then((message) => console.log(message))
+
+Output:
+// after 3s
+"Hello after 3s"
+```
+*/
+
+// My code 👇
+export function delay(time, message) {
+  // Tu código aquí 👈
+  const getMessage = new Promise(function (resolve, reject) {
+    window.setTimeout(() => {
+      resolve(message);
+    }, time);
+  });
+
+  return getMessage;
+}
+
+// Solution
+export function delay(time, message) {
+  return new Promise((resolve) => {
+    window.setTimeout(() => {
+      resolve(message);
+    }, time);
+  });
+}
