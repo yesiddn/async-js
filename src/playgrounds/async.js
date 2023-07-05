dde7c711e67b7d72f23601715e401546ac732e77
Yesid <yesidrodriguez305@gmail.com>
2023-07-04 21:29:46 -0500

    Add async function playground

---
+++ src/playgrounds/async.js
@@ -0,0 +1,31 @@
+/*
+En este desafío vas a conectarte a una API que no existe, por ende debes capturar el error haciendo uso de try/catch y lanzar un error con el mensaje API Not Found.
+
+Para lanzar el error debes usar el siguiente bloque de código:
+
+throw new Error('API Not Found');
+
+Para solucionarlo vas a encontrar una función llamada runCode que no recibe parámetros de entrada, dentro del cuerpo de la función runCode debes escribir tu solución.
+
+Ejemplo:
+
+```js
+Input:
+await runCode();
+
+Output:
+Error: API Not Found
+```
+*/
+
+export async function runCode() {
+  // Tu código aquí 👈
+  const url = 'https://domain-api-com';
+  try {
+    await fetch(url);
+  } catch (error) {
+    throw new Error('API Not Found');
+  }
+}
+
+runCode();
